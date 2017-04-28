edcaa6952a98c33a167f00909a08d9648dee03c1
Kevin <616651408@qq.com>
2017-04-28 19:23:00 +0800

    Updated changed regular

--- douban/spiders/doubanMovie.py
+++ douban/spiders/doubanMovie.py
@@ -2,7 +2,6 @@
 import scrapy
 
 from douban.movie import Movie
-import re
 
 
 class DoubanMovieSpider(scrapy.Spider):
@@ -14,13 +13,11 @@ class DoubanMovieSpider(scrapy.Spider):
 
     def parse(self, response):
         item = Movie()
-        response.sdfjklsjdflsjkflksdjfklsfj()
-        for sel in response.xpath('//*[@id="content"]/div/div[1]/ol/li/div'):
+        for sel in response.xpath('//div[@class="item"]'):
             item['title'] = sel.xpath('div/a/img/@alt').extract_first()
             item['pic'] = sel.xpath('div/a/img/@src').extract_first()
             item['link'] = sel.xpath('div/a/@href').extract_first()
             item['info'] = sel.xpath('div[2]/div[2]/p[2]/span/text()').extract_first()
             item['score'] = sel.xpath('div[2]/div[2]/div/span[2]/text()').extract_first()
-            item['commentsNum'] = sel.xpath('div[2]/div[2]/div/span[4]/text()').extract_first()
-            item['commentsNum'] = re.match('[0-9]+', item['commentsNum']).group()
+            item['commentsNum'] = sel.xpath('div[2]/div[2]/div/span[4]/text()').re(r'\d+')[0]
             yield item
